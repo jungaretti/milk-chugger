639aba62fbddfdfaa8167a8d524ece19823e9ec8
JP Ungaretti <hello@jungaretti.com>
2020-11-21 22:54:36 -0600

    Use new Login component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,19 +1,16 @@
-import Button from "react-bootstrap/Button";
+import Login from "./Login";
 import ProfileConsumption from "./ProfileConsumption";
 import ProfileHistory from "./ProfileHistory";
 import ProfileStatistics from "./ProfileStatistics";
 
-export default function Profile({ history, username }) {
+export default function Profile({ history }) {
   const gallons = Object.values(history).reduce(
     (total, current) => total + current
   );
 
   return (
     <div>
-      <Button variant="primary">Primary</Button>
-      <div>
-        <p>Hello, {username}</p>
-      </div>
+      <Login />
       <ProfileConsumption gallons={gallons} />
       <ProfileStatistics gallons={gallons} />
       <ProfileHistory history={history} />
