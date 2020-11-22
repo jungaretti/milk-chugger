8b393a3b93b5a2a47acee3a4b88de6ba33d3a312
JP Ungaretti <hello@jungaretti.com>
2020-11-21 20:47:12 -0600

    Test GlobalTotal in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import GlobalTotals from "./components/GlobalTotals";
+
+const regions = {};
+regions["Wisconsin"] = 56;
+
+const users = {};
+users["BadgerGranda"] = 56;
 
 function App() {
-  return null;
+  return <GlobalTotals gallons={56} regions={regions} users={users} />;
 }
 
 export default App;
