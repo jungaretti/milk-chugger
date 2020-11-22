550ae8ba8eb951f006e38e496992aa7f8594aaea
JP Ungaretti <hello@jungaretti.com>
2020-11-21 18:42:33 -0600

    Initialize firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
+import firebase from "firebase/app";
+import "firebase/auth";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDTgXrEUqFh1_g-7UxZ3zpJVMrwjw6rviM",
+  authDomain: "blazin-disco.firebaseapp.com",
+  databaseURL: "https://blazin-disco.firebaseio.com",
+  projectId: "blazin-disco",
+  storageBucket: "blazin-disco.appspot.com",
+  messagingSenderId: "394649767446",
+  appId: "1:394649767446:web:3dc9e36b56b4282ba1af37",
+});
+
 function App() {
   return null;
 }
