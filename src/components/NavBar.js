b9424e92cb32d68e8bb90b9e23ad987a5dc27642
JP Ungaretti <hello@jungaretti.com>
2020-12-14 16:48:35 -0600

    Use LinkContainer instead of href

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,23 @@
 import { Navbar, Nav, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function NavBar({ logInWithGoogle, signOut, user }) {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/">Milk Chugger</Navbar.Brand>
+      <LinkContainer to="/">
+        <Navbar.Brand>Milk Chugger</Navbar.Brand>
+      </LinkContainer>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/">Dashboard</Nav.Link>
-        </Nav>
+        <Nav className="mr-auto"></Nav>
         {user ? (
           <Nav>
-            <Nav.Link href="add">Drink</Nav.Link>
-            <Nav.Link href="profile">Profile</Nav.Link>
+            <LinkContainer to="/add">
+              <Nav.Link>Add</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="profile">
+              <Nav.Link>Profile</Nav.Link>
+            </LinkContainer>
             <Button onClick={signOut}>Sign Out</Button>
           </Nav>
         ) : (
