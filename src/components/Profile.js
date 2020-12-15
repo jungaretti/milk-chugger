e820d4766eded019ccc0192835a99c14f4be000f
JP Ungaretti <hello@jungaretti.com>
2020-12-14 21:29:57 -0600

    Move profile and history into divs

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,19 @@
 export default function Profile({ email, name, state, milks }) {
   return (
     <div>
-      <h3>Profile</h3>
-      <p>{email}</p>
-      <p>{name}</p>
-      <p>{state}</p>
-      <h3>History</h3>
-      {milks &&
-        milks.map((milk) => <p>Date: {milk.date.toDate().toLocaleString()}</p>)}
+      <div>
+        <h3>Profile</h3>
+        <p>{email}</p>
+        <p>{name}</p>
+        <p>{state}</p>
+      </div>
+      <div>
+        <h3>History</h3>
+        {milks &&
+          milks.map((milk) => (
+            <p>Date: {milk.date.toDate().toLocaleString()}</p>
+          ))}
+      </div>
     </div>
   );
 }
